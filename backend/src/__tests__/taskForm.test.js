d666d5387058d8fafc9f3f780e88cffa4626ac6f
Pacheco-15 <kauanpachequinho122@gmail.com>
2026-04-23 16:54:40 -0300

    adicionando caso de teste da taskForm

---
+++ backend/src/__tests__/taskForm.test.js
@@ -0,0 +1,8 @@
+const request = require('supertest');
+const app = require('../index');
+const db = require('../db');
+const bcrypt = require('bcryptjs');
+
+describe("Teste de validação do formulario", () => {
+    test
+}
